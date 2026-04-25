df0619dfef354d1b74d04320a0180120d20f81ab
lnlzycbx <lnlzycbx@users.noreply.github.com>
2026-04-25 16:38:22 +0800

    修复保存功能

--- preload.js
+++ preload.js
@@ -1,53 +1,38 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const path = require('path');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    onDocumentNew: (callback) => {
-        ipcRenderer.on('document:new', callback);
+    onMenuNew: (callback) => {
+        ipcRenderer.on('menu:new', callback);
     },
-    onDocumentOpen: (callback) => {
-        ipcRenderer.on('document:open', (event, content, filePath) => {
+    onMenuSave: (callback) => {
+        ipcRenderer.on('menu:save', callback);
+    },
+    onMenuSaveAs: (callback) => {
+        ipcRenderer.on('menu:saveAs', callback);
+    },
+    onFileOpened: (callback) => {
+        ipcRenderer.on('file:opened', (event, content, filePath) => {
             callback(content, filePath);
         });
     },
-    onGetContent: (callback) => {
-        ipcRenderer.on('document:getContent', (event, action) => {
-            callback(action);
-        });
-    },
-    onInsertImage: (callback) => {
-        ipcRenderer.on('image:insert', (event, imagePath) => {
+    onImageInserted: (callback) => {
+        ipcRenderer.on('image:inserted', (event, imagePath) => {
             callback(imagePath);
         });
     },
-    sendContent: (content, images) => {
-        ipcRenderer.send('document:content', content, images);
-    },
-    notifyModified: () => {
-        ipcRenderer.send('document:modified');
-    },
-    getFileName: (filePath) => {
-        return path.basename(filePath);
-    },
-    dialogOpenFile: async () => {
+    openFile: async () => {
         return await ipcRenderer.invoke('dialog:openFile');
     },
-    dialogSaveFile: async (content, images) => {
-        return await ipcRenderer.invoke('dialog:saveFile', content, images);
+    saveFile: async (content, images, useCurrentPath) => {
+        return await ipcRenderer.invoke('dialog:saveFile', content, images, useCurrentPath);
     },
-    dialogSaveAsFile: async (content, images) => {
-        return await ipcRenderer.invoke('dialog:saveAsFile', content, images);
-    },
-    dialogInsertImage: async () => {
+    insertImage: async () => {
         return await ipcRenderer.invoke('dialog:insertImage');
     },
-    newDocument: async () => {
-        return await ipcRenderer.invoke('document:new');
+    getFileName: async (filePath) => {
+        return await ipcRenderer.invoke('path:basename', filePath);
     },
     getCurrentFilePath: async () => {
         return await ipcRenderer.invoke('get:currentFilePath');
-    },
-    removeAllListeners: (channel) => {
-        ipcRenderer.removeAllListeners(channel);
     }
 });
